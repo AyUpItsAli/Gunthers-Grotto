1b59e23ffe7a8a8d9c597108cb12b5f7477033e9
Ali <ali.dee1410@gmail.com>
2024-07-07 12:44:24 +0100

    Rename camera settings

--- scenes/3d/world/dev/prototype/camera.gd
+++ scenes/3d/world/dev/prototype/camera.gd
@@ -2,14 +2,14 @@
 extends Camera3D
 
 @export var target: Node3D
-@export var height: float = 6
-@export var distance: float = 10
+@export var y_offset: float = 6
+@export var z_offset: float = 10
 @export var smooth_weight: float = 0.1
 
 func _physics_process(_delta):
 	if not target or target.is_queued_for_deletion():
 		return
 	position.x = lerp(position.x, target.position.x, smooth_weight)
-	position.y = height
-	position.z = target.position.z + distance
+	position.y = target.position.z + y_offset
+	position.z = target.position.z + z_offset
 	look_at(Vector3(position.x, target.position.y, target.position.z))
