b7dc6c10f02fce61fdb6a52457b62546b1cf1709
Ali <ali.dee1410@gmail.com>
2024-07-08 19:26:44 +0100

    Foreshortening correction can be toggled

--- scenes/3d/world/dev/prototype/viewport_container.gd
+++ scenes/3d/world/dev/prototype/viewport_container.gd
@@ -2,15 +2,21 @@
 extends SubViewportContainer
 
 @warning_ignore("unused_private_class_variable")
-@export var _correct_foreshortening: bool = true:
+@export var _update_viewport: bool = true:
 	set(new_value):
-		correct_foreshortening()
+		_update_viewport = true
+		update_viewport()
 @export var viewport: SubViewport
 @export var camera: Camera3D
+@export var foreshortening_correction: bool = true
 
-func correct_foreshortening():
-	scale.y = sqrt(2)
-	position.y = -(size.y * (sqrt(2)-1)) / 2
+func update_viewport():
+	if foreshortening_correction:
+		scale.y = sqrt(2)
+		position.y = -(size.y * (sqrt(2)-1)) / 2
+	else:
+		scale.y = 1
+		position.y = 0
 
 func _ready():
-	correct_foreshortening()
+	update_viewport()
