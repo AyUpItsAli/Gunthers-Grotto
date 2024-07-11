8eb3c246fa356906596861c2811b7344dd20e43c
Ali <ali.dee1410@gmail.com>
2024-07-11 15:36:29 +0100

    Wall tile previewer
    - Moved logic for updating wall tile in the editor to dedicated previewer scene
     - This makes wall tile updates a bit faster in game
    - Moved level previewer and wall tile previewer to preview folder

---
+++ scenes/dev/preview/wall_tile_previewer.gd
@@ -0,0 +1,33 @@
+@tool
+extends Node
+
+@export var wall_tile: WallTile
+@warning_ignore("unused_private_class_variable")
+@export var _redraw: bool = true:
+	set(new_value):
+		_redraw = true
+		redraw()
+@export var tile_set: WallTileSet:
+	set(new_value):
+		if tile_set == new_value: return
+		if tile_set: tile_set.changed.disconnect(redraw)
+		tile_set = new_value
+		if tile_set: tile_set.changed.connect(redraw)
+		redraw()
+@export var atlas_coords: Vector2i = Vector2i(0, 3):
+	set(new_value):
+		atlas_coords = new_value
+		redraw()
+
+
+func redraw():
+	if not wall_tile: return
+	var shape = wall_tile.collision_shape.shape as BoxShape3D
+	shape.size.y = 1
+	wall_tile.collision_shape.position.y = 0.5
+	wall_tile.sprite_top.texture = null
+	wall_tile.sprite_bottom.texture = null
+	if not tile_set: return
+	wall_tile.tile_set = tile_set
+	wall_tile.atlas_coords = atlas_coords
+	wall_tile.setup_tile()
--- scenes/world/objects/wall_tile/wall_tile.gd
+++ scenes/world/objects/wall_tile/wall_tile.gd
@@ -2,53 +2,35 @@
 class_name WallTile
 extends StaticBody3D
 
-@export var tile_set: WallTileSet:
-	set(new_value):
-		if tile_set == new_value: return
-		if tile_set: tile_set.changed.disconnect(redraw)
-		tile_set = new_value
-		if tile_set: tile_set.changed.connect(redraw)
-		redraw()
-@export var atlas_coords: Vector2i = Vector2i(0, 3):
-	set(new_value):
-		atlas_coords = new_value
-		redraw()
+@export var tile_set: WallTileSet
+@export var atlas_coords: Vector2i = Vector2i(0, 3)
 @export var collision_shape: CollisionShape3D
 @export var sprite_top: Sprite3D
 @export var sprite_bottom: Sprite3D
 
-func redraw():
-	if Engine.is_editor_hint():
-		update_tile()
-
-func update_tile():
-	if not collision_shape: return
-	var shape = collision_shape.shape as BoxShape3D
-	shape.size.y = 1
-	collision_shape.position.y = 0.5
-	if not sprite_top: return
-	sprite_top.texture = null
-	if not sprite_bottom: return
-	sprite_bottom.texture = null
-	if not tile_set: return
-	
+func setup_tile():
 	# Collision Shape
+	var shape = collision_shape.shape as BoxShape3D
 	shape.size.y = tile_set.wall_height
 	collision_shape.position.y = tile_set.wall_height * 0.5
-	
 	# Top Sprite
 	sprite_top.texture = tile_set.texture
 	sprite_top.region_rect.size = Vector2(Globals.TILE_SIZE, Globals.TILE_SIZE)
-	sprite_top.region_rect.position.x = atlas_coords.x * Globals.TILE_SIZE
-	sprite_top.region_rect.position.y = atlas_coords.y * (tile_set.wall_height + 1) * Globals.TILE_SIZE
 	sprite_top.position.y = tile_set.wall_height
-	
 	# Bottom Sprite
 	sprite_bottom.texture = tile_set.texture
 	sprite_bottom.region_rect.size = Vector2(Globals.TILE_SIZE, Globals.TILE_SIZE * tile_set.wall_height)
+	sprite_bottom.position.y = tile_set.wall_height * 0.5
+	# Texture coords
+	update_texture_coords()
+
+func update_texture_coords():
+	# Top Sprite
+	sprite_top.region_rect.position.x = atlas_coords.x * Globals.TILE_SIZE
+	sprite_top.region_rect.position.y = atlas_coords.y * (tile_set.wall_height + 1) * Globals.TILE_SIZE
+	# Bottom Sprite
 	sprite_bottom.region_rect.position.x = atlas_coords.x * Globals.TILE_SIZE
 	sprite_bottom.region_rect.position.y = ((atlas_coords.y * (tile_set.wall_height + 1)) + 1) * Globals.TILE_SIZE
-	sprite_bottom.position.y = tile_set.wall_height * 0.5
 
 func _ready():
-	update_tile()
+	setup_tile()
