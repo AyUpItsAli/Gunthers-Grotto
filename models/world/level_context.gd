95ee53681256f60c97ad74bb832a96f71931de57
Ali <ali.dee1410@gmail.com>
2024-07-12 14:18:40 +0100

    LevelContext reformatting + todo comment

--- models/world/level_context.gd
+++ models/world/level_context.gd
@@ -36,11 +36,12 @@ func get_random_tile() -> Vector2i:
 	return Vector2i(rng.randi_range(start_x, end_x), rng.randi_range(start_y, end_y))
 
 func get_random_empty_tile() -> Vector2i:
-	var tile := get_random_tile()
+	var tile: Vector2i = get_random_tile()
 	while not is_tile_empty(tile):
 		tile = get_random_tile()
 	return tile
 
+# TODO: Update this to work for the new 3d world, ie: make a 3d version of "map_to_local"
 func get_random_unoccupied_pos() -> Vector2:
-	var empty_tile := get_random_empty_tile()
+	var empty_tile: Vector2i = get_random_empty_tile()
 	return tile_map.map_to_local(empty_tile)
