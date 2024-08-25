fd03f7646c7c05e92b9f7169fe2bb261c552ed5f
Ali <ali.dee1410@gmail.com>
2024-08-25 14:18:23 +0100

    Basic pickaxe rotation
    - Finished basic logic for hand node in player scene, used to rotate the held item (the pickaxe)
    - Added sword asset for testing longer weapon sprites
    - Set alpha cut to disabled for ground tile and pickaxe sprite. Hopefully this won't cause issues later...
    - Set render priority for ground tile to -1 (so ground renders below pickaxe)
    I'm not 100% happy with this system yet, but it's been a pain and this method is bearable right now.

--- scenes/actors/player/hand.gd
+++ scenes/actors/player/hand.gd
@@ -2,61 +2,32 @@
 class_name Hand
 extends Node3D
 
-enum Axis {
-	Z_AXIS,
-	Y_AXIS
-}
-
-## Axis in which the pickaxe faces
-@export var axis: Axis
-## The current angle (in degrees) at which the hand is rotated
-@export_range(0, 360) var angle: float = 0
-## The height (in meters) at which the pickaxe will no longer clip through the floor
-@export var height: float = 1.5
-## The distance (in meters) at which the pickaxe will be from the player
-@export var distance: float = 2
-## The height (in pixels) at which the pickaxe will appear to pivot around
-@export var pixel_height: float = 6
+@export_range(0, 360) var angle: float = 0:
+	set(new_value):
+		angle = new_value
+		update_hand()
+@export_range(0, 20) var body_radius: float = 6:
+	set(new_value):
+		body_radius = distance if new_value > distance else new_value
+@export_range(0, 20) var distance: float = 12:
+	set(new_value):
+		distance = body_radius if new_value < body_radius else new_value
+@export_range(0, 20) var height: float = 5
 @export_group("Nodes")
 @export var pickaxe: Node3D
 @export var pickaxe_sprite: Sprite3D
 @export var hitbox_pivot: Node3D
 
-func _physics_process(_delta: float) -> void:
-	# Currently testing out 2 different methods:
-	# Rotation in the Z axis and rotation in the Y axis
-	match axis:
-		Axis.Z_AXIS:
-			rotate_z_axis()
-		Axis.Y_AXIS:
-			rotate_y_axis()
-
-func rotate_z_axis() -> void:
-	position.y = height
-	position.z = height - (pixel_height * Globals.PIXEL_SIZE)
+func update_hand() -> void:
+	# Offset pickaxe from hand origin by distance
+	pickaxe.position.z = distance * Globals.PIXEL_SIZE
+	# Position hand origin
+	position.z = pickaxe.position.z - (body_radius * Globals.PIXEL_SIZE)
+	position.y = position.z + (height * Globals.PIXEL_SIZE)
+	# Rotate hand
 	rotation_degrees.y = angle
-	
+	# Rotate pickaxe to match hand rotation
 	pickaxe.rotation_degrees.y = -angle
 	pickaxe.rotation_degrees.z = angle
-	pickaxe.position.z = distance - position.z
-	
-	pickaxe_sprite.axis = Vector3.AXIS_Z
-	pickaxe_sprite.rotation_degrees.y = 0
-	pickaxe_sprite.rotation_degrees.z = -135
-	
-	hitbox_pivot.rotation_degrees.y = angle
-
-func rotate_y_axis() -> void:
-	position.y = pixel_height * Globals.PIXEL_SIZE
-	position.z = 0
-	rotation_degrees.y = angle
-	
-	pickaxe.rotation_degrees.y = 0
-	pickaxe.rotation_degrees.z = 0
-	pickaxe.position.z = distance
-	
-	pickaxe_sprite.axis = Vector3.AXIS_Y
-	pickaxe_sprite.rotation_degrees.y = -135
-	pickaxe_sprite.rotation_degrees.z = 0
-	
+	# Rotate hitbox
 	hitbox_pivot.rotation_degrees.y = angle
